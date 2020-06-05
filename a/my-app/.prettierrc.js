03cfecfa20892b3b167a6fa3ba007808afa104aa
yulei0421 <1300381123@qq.com>
2020-06-05 11:47:54 +0800

    update

---
+++ a/my-app/.prettierrc.js
@@ -0,0 +1,21 @@
+/*
+ * @Author:余磊
+ * @Date: 2020-06-05 11:43:53
+ * @LastEditTime: 2020-06-05 11:45:31
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /my-app/.prettierrc.js
+ */
+
+module.exports = {
+    printWidth: 120, // 换行字符串阈值
+    semi: true, // 句末加分号
+    singleQuote: true, // 用单引号
+    trailingComma: 'none', // 最后一个对象元素加逗号
+    bracketSpacing: true, // 对象，数组加空格
+    jsxBracketSameLine: false, // jsx > 是否另起一行
+    arrowParens: 'always', // (x) => {} 是否要有小括号
+    requirePragma: false, // 是否要注释来决定是否格式化代码
+    proseWrap: 'preserve', // 是否要换行
+    tabWidth: 4 //缩进
+};
